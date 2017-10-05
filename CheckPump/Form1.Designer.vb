425767733ba4d86e52177dbc417ea07b283000f0
anongnuch <kogkag@gmail.com>
2017-10-05 14:00:25 +0700

    Add File To git

---
+++ CheckPump/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Gray
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(498, 50)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CHECK PUMP"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCheck
+        '
+        Me.btnCheck.BackColor = System.Drawing.Color.LimeGreen
+        Me.btnCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnCheck.Location = New System.Drawing.Point(102, 82)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(122, 51)
+        Me.btnCheck.TabIndex = 1
+        Me.btnCheck.Text = "ตรวจสอบ"
+        Me.btnCheck.UseVisualStyleBackColor = False
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(253, 82)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(122, 51)
+        Me.btnCancel.TabIndex = 2
+        Me.btnCancel.Text = "ยกเลิก"
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(493, 194)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CHECK PUMP"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents btnCancel As Button
+End Class
---
+++ CheckPump/Form1.vb
@@ -0,0 +1,141 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim conn As New SqlConnection(ConnStr)
+            conn.Open()
+        Catch ex As Exception
+            MsgBox("ไม่สามารถเชื่อมต่อฐานข้อมูลได้", MsgBoxStyle.OkOnly)
+            Application.Exit()
+        End Try
+    End Sub
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        Try
+
+            If CheckIsExistDeleted() = True Then
+                Using New Centered_MessageBox(Me)
+                    Dim confirm As DialogResult = MessageBox.Show("มีการตั้งค่าไม่ถูกต้อง", "", MessageBoxButtons.OK)
+                    Exit Sub
+                End Using
+            End If
+
+
+            If CheckPump() = False Then
+                Using New Centered_MessageBox(Me)
+                    Dim confirm As DialogResult = MessageBox.Show("มีการตั้งค่าไม่ถูกต้อง", "", MessageBoxButtons.OK)
+                    Exit Sub
+                End Using
+            End If
+
+            Using New Centered_MessageBox(Me)
+                Dim confirm As DialogResult = MessageBox.Show("ตั้งค่าถูกต้องแล้ว", "", MessageBoxButtons.OK)
+                Exit Sub
+            End Using
+
+        Catch ex As Exception
+            Using New Centered_MessageBox(Me)
+                Dim confirm As DialogResult = MessageBox.Show(ex.ToString, "", MessageBoxButtons.OK)
+            End Using
+        End Try
+    End Sub
+
+    Function CheckIsExistDeleted() As Boolean
+        'True = Exist, False = Not
+
+        Dim dt As New DataTable
+        Dim sql As String = "select 'y' from Hoses where deleted = '1'"
+        Dim conn As New SqlConnection(ConnStr)
+        conn.Open()
+        Dim cmd As New SqlCommand
+        With cmd
+            .CommandText = sql
+            .CommandType = CommandType.Text
+            .Connection = conn
+        End With
+
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        conn.Close()
+
+        If dt.Rows.Count = 0 Then
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Function CheckPump() As Boolean
+        'True = Not Problem, False = Problem
+        Dim dt As New DataTable
+        Dim sql As String = "select Hose_ID,Pump_ID from Hoses order by hose_ID"
+        Dim conn As New SqlConnection(ConnStr)
+        conn.Open()
+        Dim cmd As New SqlCommand
+        With cmd
+            .CommandText = sql
+            .CommandType = CommandType.Text
+            .Connection = conn
+        End With
+
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        conn.Close()
+
+        If dt.Rows.Count = 0 Then
+            Return False
+        End If
+
+
+        Dim tempPump As Integer = 1
+        For i As Integer = 0 To dt.Rows.Count - 1
+            Dim pump As Integer = CInt(dt.Rows(i)("Pump_ID"))
+            Dim hose As Integer = CInt(dt.Rows(i)("Hose_ID"))
+
+            If hose <> i + 1 Then
+                Return False
+            End If
+
+            If tempPump <> pump Then
+                If tempPump + 1 <> pump Then
+                    Return False
+                Else
+                    tempPump = pump
+                End If
+            End If
+
+        Next
+        Return True
+    End Function
+
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Application.Exit()
+    End Sub
+
+
+#Region "Connect Database"   'Connect Database
+    'Dim Server As String = "10.195.2.177"
+    'Dim Password As String = "pTT!CT01"
+    Public ConnStr As String = getConnectionString()
+    Function getConnectionString() As String
+
+        Dim Server As String = "(local)"
+        'Dim Server As String = "10.195.2.177"
+
+        Dim Database As String = "ENABLERDB"
+        Dim Username As String = "sa"
+
+        'Dim Password As String = "1qaz@WSX"
+        Dim Password As String = "pTT!CT01"
+
+
+        'Dim ini As New IniReader(INIFile)
+        'ini.Section = "Setting"
+        Return "Data Source=" & Server & ";Initial Catalog=" & Database & ";User ID=" & Username & ";Password=" & Password & ";Connect Timeout=1;"
+    End Function
+
+#End Region
+
+End Class
